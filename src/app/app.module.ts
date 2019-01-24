7cc8e59e42668a93c6af104bb196efdf74e5081e
Basvanderhoeven <basvdhoevenn@gmail.com>
2019-01-24 13:46:21 +0100

    Kludde website 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,69 @@
+import { NgtUniversalModule } from '@ng-toolkit/universal';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { RouterModule, Routes, Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GeschiedenisComponent } from './geschiedenis/geschiedenis.component';
+import { PraesidiumComponent } from './praesidium/praesidium.component';
+import { PartnersComponent } from './partners/partners.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SEOService } from './seoservice.service';
+import { Title, Meta } from '@angular/platform-browser';
+import { map, filter, scan } from 'rxjs/operators';
+
+const appRoutes: Routes = [
+  { 
+    path: 'home', 
+    component: HomeComponent,
+    data: { title: 'Kludde | Home', metaDescription: 'Welkom op de website van Kludde!' }
+   },
+  { 
+    path: 'geschiedenis', 
+    component: GeschiedenisComponent,
+    data: { title: 'Kludde | Geschiedenis', metaDescription: 'Meer over de geschiedenis van Kludde' }
+   },
+  { 
+    path: 'praesidium', 
+    component: PraesidiumComponent,
+    data: { title: 'Kludde | Praesidium', metaDescription: 'Leer ons team kennen!' }
+  },
+  { 
+    path: 'partners', 
+    component: PartnersComponent,
+    data: { title: 'Kludde | Partners', metaDescription: 'De partners van Kludde' }
+  },
+  { 
+    path: '**', 
+    component: HomeComponent,
+    data: { title: 'Page not found', metaDescription: 'Deze pagina kan niet worden gevonden' }
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GeschiedenisComponent,
+    PraesidiumComponent,
+    PartnersComponent,
+    PageNotFoundComponent
   ],
-  imports: [
-    BrowserModule
+  imports:[
+    CommonModule,
+    NgtUniversalModule,
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [SEOService],
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute) {}
+    ngOnInit() {
+      
+  }
+}
---
+++ src/app/praesidium/praesidium.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-praesidium',
+  templateUrl: './praesidium.component.html',
+  styleUrls: ['./praesidium.component.css']
+})
+export class PraesidiumComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
